4d85ca9f86287c3639974330ec9fead862610ed6
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-21 10:40:08 +0200

    [FIX] Correzione esercizio

--- js/main.js
+++ js/main.js
@@ -5,10 +5,13 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            titolo: 'Ciao, sono Vue.JS',
-            immagine: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLKWXv3sOTG8LgqcOG7zhlAFctu_wxM2XO-Qj1KdKSWPszOd3MLW3tMgau0bxOFCYV4KM&usqp=CAU'
+            titolo: '',
+            immagine: ''
         }
     },
 
-
+    created(){
+        this.titolo = 'Ciao, sono Vue.JS';
+        this.immagine = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLKWXv3sOTG8LgqcOG7zhlAFctu_wxM2XO-Qj1KdKSWPszOd3MLW3tMgau0bxOFCYV4KM&usqp=CAU'
+    }
 }).mount('#app');
